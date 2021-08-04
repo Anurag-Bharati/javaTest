fcf5f91f957b8c8163e3d0fce4f3c06ff09ea468
Sanif <sanifkandel12345@gmail.com>
2021-08-04 21:33:20 +0545

    Don

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("This is first test");
         System.out.println("This is a test");
     }
 }
